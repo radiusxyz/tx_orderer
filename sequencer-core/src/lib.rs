723186481068987f8fa8cdfd7a89ab217140964a
Kanet <zombiedelah@gmail.com>
2024-04-09 16:25:42 +0900

    re-export tracing macros from the top level

--- sequencer-core/src/lib.rs
+++ sequencer-core/src/lib.rs
@@ -14,5 +14,7 @@ pub use serde_json;
 pub use tokio;
 pub use tower;
 pub use tower_http;
-pub use tracing;
+pub use tracing::{
+    debug as print_debug, error as print_error, info as print_info, warn as print_warn,
+};
 pub use tracing_subscriber;
--- sequencer-core/src/macros/mod.rs
+++ sequencer-core/src/macros/mod.rs
@@ -1,6 +1,3 @@
 pub mod bound;
 pub mod caller;
 pub mod unrecoverable;
-pub use tracing::{
-    debug as print_debug, error as print_error, info as print_info, warn as print_warn,
-};
