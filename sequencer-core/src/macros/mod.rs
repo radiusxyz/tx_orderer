62ac4048ad54a1b33329a21153ecfa0849765774
Kanet <zombiedelah@gmail.com>
2024-04-09 16:20:36 +0900

    Avoid namespace collision

--- sequencer-core/src/macros/mod.rs
+++ sequencer-core/src/macros/mod.rs
@@ -1,4 +1,6 @@
 pub mod bound;
 pub mod caller;
 pub mod unrecoverable;
-pub use tracing::{debug, error, info, warn};
+pub use tracing::{
+    debug as print_debug, error as print_error, info as print_info, warn as print_warn,
+};
