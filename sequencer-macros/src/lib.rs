7bbd642af1ae08b049ca07c561f372b65e3721b6
kanet <zombiedelah@gmail.com>
2024-05-12 16:29:20 +0900

    WIP: attribute 'data'

--- sequencer-macros/src/lib.rs
+++ sequencer-macros/src/lib.rs
@@ -1,20 +1,20 @@
 pub(crate) mod attribute_data;
-pub(crate) mod event;
+// pub(crate) mod event;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput, Error};
 
 #[proc_macro_attribute]
 pub fn data(metadata: TokenStream, input: TokenStream) -> TokenStream {
-    data::expand_attribute_data(metadata.into(), input.into())
+    attribute_data::expand_attribute_data(metadata.into(), input.into())
         .unwrap_or_else(Error::into_compile_error)
         .into()
 }
 
-#[proc_macro_derive(Event)]
-pub fn derive_event(input: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(input as DeriveInput);
-    event::expand_derive_event(&mut input)
-        .unwrap_or_else(Error::into_compile_error)
-        .into()
-}
+// #[proc_macro_derive(Event)]
+// pub fn derive_event(input: TokenStream) -> TokenStream {
+//     let mut input = parse_macro_input!(input as DeriveInput);
+//     event::expand_derive_event(&mut input)
+//         .unwrap_or_else(Error::into_compile_error)
+//         .into()
+// }
