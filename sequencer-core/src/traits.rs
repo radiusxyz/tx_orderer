518707fc8983659fb3d4e32cc668c3a063e0544f
Kanet <zombiedelah@gmail.com>
2024-05-16 18:51:16 +0900

    'Data' as a helper trait

--- sequencer-core/src/traits.rs
+++ sequencer-core/src/traits.rs
@@ -2,10 +2,10 @@ use std::fmt::Debug;
 
 use serde::{de::DeserializeOwned, ser::Serialize};
 
-pub trait Data: Clone + Debug + DeserializeOwned + Serialize {
-    fn id() -> &'static str;
-}
+pub trait Data: Clone + Debug + DeserializeOwned + Serialize {}
 
-pub trait Event: Data {
-    fn id(&self) -> &'static str;
+pub trait Event: Send + 'static {
+    fn id(&self) -> &'static str
+    where
+        Self: Sized;
 }
