75dbb7520904c0a682505baef8e8c4b0b0417f40
kanet <zombiedelah@gmail.com>
2024-05-29 18:48:55 +0900

    Refactor dependencies

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -6,11 +6,8 @@ pub use bincode;
 pub use hyper;
 pub use jsonrpsee;
 pub use rand;
-pub use rocksdb;
 pub use serde;
 pub use serde_json;
-pub use tower;
-pub use tower_http;
 pub use tracing::{
     debug as print_debug, error as print_error, info as print_info, warn as print_warn,
 };
--- crates/ssal/src/event.rs
+++
@@ -1,4 +0,0 @@
-pub enum Event {
-    RegisteredSequencer,
-    DeregisteredSequencer,
-}
