55aaa6a617cbe183432c7661be8f37bf53d03271
kanet <zombiedelah@gmail.com>
2024-06-03 20:42:47 +0900

    Add feature = [singleton]

--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -1,7 +1,11 @@
 mod client;
 mod error;
 mod lock;
+#[cfg(feature = "singleton")]
+mod singleton;
 
 pub use client::Database;
 pub use error::{Error, ErrorKind};
 pub use lock::Lock;
+#[cfg(feature = "singleton")]
+pub use singleton::*;
