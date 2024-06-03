e3d50cdf12c803e043136a729d27b98a7a34b4af
kanet <zombiedelah@gmail.com>
2024-06-03 14:28:14 +0900

    Remove old error

--- crates/database/src/error.rs
+++ crates/database/src/error.rs
@@ -73,24 +73,3 @@ impl Error {
         self.kind
     }
 }
-
-// #[derive(Debug)]
-// pub enum Error {
-//     Open(rocksdb::Error),
-//     Get(rocksdb::Error),
-//     GetMut(rocksdb::Error),
-//     Put(rocksdb::Error),
-//     Delete(rocksdb::Error),
-//     Commit(rocksdb::Error),
-//     SerializeKey(bincode::Error),
-//     SerializeValue(bincode::Error),
-//     DeserializeValue(bincode::Error),
-// }
-
-// impl std::fmt::Display for Error {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{:?}", self)
-//     }
-// }
-
-// impl std::error::Error for Error {}
