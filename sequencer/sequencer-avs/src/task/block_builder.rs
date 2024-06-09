d6152cbdd2a6417667806c3af4ea22b8fea2c945
kanet <zombiedelah@gmail.com>
2024-06-09 10:53:13 +0900

    syncers and builder

--- sequencer/sequencer-avs/src/task/block_builder.rs
+++ sequencer/sequencer-avs/src/task/block_builder.rs
@@ -1,6 +1,6 @@
 use crate::types::*;
 
-pub fn init(rollup_block_number: RollupBlockNumber) {
+pub fn init(rollup_block_number: RollupBlockNumber, register_block_commitment: bool) {
     let previous_rollup_block_number = rollup_block_number - 1;
     let previous_block_height: u64 = match BlockMetadata::get(previous_rollup_block_number).ok() {
         Some(block_metadata) => block_metadata.block_height(),
@@ -8,15 +8,18 @@ pub fn init(rollup_block_number: RollupBlockNumber) {
     };
 
     tokio::spawn(async move {
-        let mut block = Vec::<ProcessedTransaction>::with_capacity(previous_block_height as usize);
+        let mut block = Block::new(previous_block_height as usize);
         for transaction_order in 0..previous_block_height {
-            let processed_transaction =
-                ProcessedTransaction::get(rollup_block_number, transaction_order).unwrap();
-            block.push(processed_transaction);
+            let transaction = Transaction::get(rollup_block_number, transaction_order).unwrap();
+            block.push(transaction);
         }
+        block.put(previous_rollup_block_number).unwrap();
 
-        // TODO: Block commitment.
-        // Transaction type must implement `AsRef<[u8]>`.
-        Block::new(block).put(previous_rollup_block_number).unwrap();
+        // TODO: Change the seed to getting it from the contract.
+        let block_commitment = block.commitment([0; 32]);
+        block_commitment.put(previous_rollup_block_number).unwrap();
+
+        // TODO: Implement register_block_commitment() in the contract.
+        if register_block_commitment {}
     });
 }
