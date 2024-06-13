bd37a83d055affb68eb8880f753e457edbb87a7b
Kanet <zombiedelah@gmail.com>
2024-06-13 10:51:19 +0900

    Refactored for the new JSON-RPC API

--- seeder/seeder-avs/src/rpc/get_address_list.rs
+++ seeder/seeder-avs/src/rpc/get_address_list.rs
@@ -1,28 +1,18 @@
 use super::prelude::*;
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct GetAddressList {
-    pub sequencer_list: Vec<PublicKey>,
-}
-
-#[async_trait]
-impl RpcMethod for GetAddressList {
-    type Response = Vec<Option<RpcAddress>>;
-
-    fn method_name() -> &'static str {
-        stringify!(GetAddressList)
-    }
-
-    async fn handler(self) -> Result<Self::Response, RpcError> {
-        let sequencer_list: Vec<Option<RpcAddress>> = self
-            .sequencer_list
-            .iter()
-            .map(|sequencer_public_key| {
-                database()
-                    .get::<PublicKey, RpcAddress>(sequencer_public_key)
-                    .ok()
-            })
-            .collect();
-        Ok(sequencer_list)
-    }
+pub async fn handler(
+    parameter: RpcParameter,
+    _context: Arc<()>,
+) -> Result<Vec<Option<RpcAddress>>, RpcError> {
+    let parameter = parameter.parse::<GetAddressList>()?;
+    let sequencer_list: Vec<Option<RpcAddress>> = parameter
+        .sequencer_list
+        .iter()
+        .map(|sequencer_public_key| {
+            database()
+                .get::<PublicKey, RpcAddress>(sequencer_public_key)
+                .ok()
+        })
+        .collect();
+    Ok(sequencer_list)
 }
--- seeder/seeder-avs/src/rpc/register.rs
+++ seeder/seeder-avs/src/rpc/register.rs
@@ -1,24 +1,10 @@
 use super::prelude::*;
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Register {
-    pub public_key: PublicKey,
-    pub sequencer_rpc_address: RpcAddress,
-}
-
-#[async_trait]
-impl RpcMethod for Register {
-    type Response = ();
-
-    fn method_name() -> &'static str {
-        stringify!(Register)
-    }
-
-    async fn handler(self) -> Result<Self::Response, RpcError> {
-        tracing::info!("{:?}", self);
-        health_check(&self.sequencer_rpc_address).await?;
-        database()
-            .put(&self.public_key, &self.sequencer_rpc_address)
-            .map_err(|error| error.into())
-    }
+pub async fn handler(parameter: RpcParameter, _context: Arc<()>) -> Result<(), RpcError> {
+    let parameter = parameter.parse::<Register>()?;
+    tracing::info!("{:?}", parameter);
+    health_check(&parameter.sequencer_rpc_address).await?;
+    database()
+        .put(&parameter.public_key, &parameter.sequencer_rpc_address)
+        .map_err(|error| error.into())
 }
