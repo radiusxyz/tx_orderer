1e37099464eff26269d2ee4c679b150963973c77
Kanet <zombiedelah@gmail.com>
2024-06-14 15:48:59 +0900

    Add 'tokio::time' dependencies to implement retry functionalities

--- crates/json-rpc/src/client.rs
+++ crates/json-rpc/src/client.rs
@@ -1,15 +1,16 @@
-use std::time::Duration;
-
 use jsonrpsee::{
     core::client::ClientT,
     http_client::{HttpClient, HttpClientBuilder},
 };
 use serde::{de::DeserializeOwned, ser::Serialize};
+use tokio::time::{sleep, Duration};
 
 use crate::{types::Parameter, Error, ErrorKind};
 
 pub struct RpcClient {
     http_client: HttpClient,
+    retry: u8,
+    retry_interval: u64,
 }
 
 impl RpcClient {
@@ -19,12 +20,27 @@ impl RpcClient {
             .request_timeout(Duration::from_secs(timeout))
             .build(endpoint)
             .map_err(|error| (ErrorKind::BuildClient, error))?;
-        Ok(Self { http_client })
+        Ok(Self {
+            http_client,
+            retry: 0,
+            retry_interval: 0,
+        })
     }
 
-    pub async fn request<P, R>(&self, method: &'static str, parameter: P) -> Result<R, Error>
+    pub fn max_retry(mut self, value: u8) -> Self {
+        self.retry = value;
+        self
+    }
+
+    /// Retry interval in seconds
+    pub fn retry_interval(mut self, value: u64) -> Self {
+        self.retry_interval = value;
+        self
+    }
+
+    async fn request_inner<P, R>(&self, method: &'static str, parameter: P) -> Result<R, Error>
     where
-        P: Serialize + Send,
+        P: Clone + Serialize + Send,
         R: DeserializeOwned,
     {
         let parameter = Parameter::from(parameter);
@@ -32,5 +48,23 @@ impl RpcClient {
             .request(method, parameter)
             .await
             .map_err(|error| (ErrorKind::RpcRequest, error).into())
+    }
+
+    pub async fn request<P, R>(&self, method: &'static str, parameter: P) -> Result<R, Error>
+    where
+        P: Clone + Serialize + Send,
+        R: DeserializeOwned,
+    {
+        if self.retry != 0 {
+            for _ in 0..self.retry {
+                if let Some(response) = self.request_inner(method, parameter.clone()).await.ok() {
+                    return Ok(response);
+                } else {
+                    sleep(Duration::from_secs(self.retry_interval)).await;
+                }
+            }
+        }
+
+        self.request_inner(method, parameter).await
     }
 }
--- crates/json-rpc/src/types.rs
+++ crates/json-rpc/src/types.rs
@@ -7,11 +7,20 @@ pub type RpcParameter = Params<'static>;
 /// Wrapper for the RPC request parameter.
 pub(crate) struct Parameter<P>(P)
 where
-    P: Serialize + Send;
+    P: Clone + Serialize + Send;
+
+impl<P> Clone for Parameter<P>
+where
+    P: Clone + Serialize + Send,
+{
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
 
 impl<P> From<P> for Parameter<P>
 where
-    P: Serialize + Send,
+    P: Clone + Serialize + Send,
 {
     fn from(value: P) -> Self {
         Self(value)
@@ -20,7 +29,7 @@ where
 
 impl<P> ToRpcParams for Parameter<P>
 where
-    P: Serialize + Send,
+    P: Clone + Serialize + Send,
 {
     fn to_rpc_params(self) -> Result<Option<Box<RawValue>>, serde_json::Error> {
         let json_string = serde_json::to_string(&self.0)?;
