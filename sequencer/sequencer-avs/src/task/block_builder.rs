d0769f60c482c0854198dce59cf03a911327969b
kanet <zombiedelah@gmail.com>
2024-06-17 00:48:32 +0900

    Initial block builder

--- sequencer/sequencer-avs/src/task/block_builder.rs
+++ sequencer/sequencer-avs/src/task/block_builder.rs
@@ -1,15 +1,46 @@
-use crate::types::*;
+use json_rpc::RpcClient;
+
+use crate::{rpc::external::GetTransaction, types::*};
 
 pub fn init(
+    ssal_block_number: SsalBlockNumber,
     rollup_block_number: RollupBlockNumber,
     block_height: u64,
     register_block_commitment: bool,
 ) {
     tokio::spawn(async move {
+        let sequencer_list = SequencerList::get(ssal_block_number).unwrap();
+
+        // Build block.
         let mut block = Block::new(block_height as usize);
         for transaction_order in 0..block_height {
-            let transaction = Transaction::get(rollup_block_number, transaction_order).unwrap();
-            block.push(transaction);
+            match Transaction::get(rollup_block_number, transaction_order) {
+                Ok(transaction) => block.push(transaction),
+                Err(error) => {
+                    if error.kind() == database::ErrorKind::KeyDoesNotExist {
+                        // Fetch the missing transaction from other sequencers.
+                        let rpc_method = GetTransaction {
+                            rollup_block_number,
+                            transaction_order,
+                        };
+
+                        // Get the first OK response from the sequencer list.
+                        let transaction: Transaction = RpcClient::fetch(
+                            sequencer_list.address(),
+                            3,
+                            GetTransaction::METHOD_NAME,
+                            rpc_method,
+                        )
+                        .await
+                        .unwrap();
+
+                        block.push(transaction);
+                    } else {
+                        // Unlikely.
+                        tracing::error!("{}", error);
+                    }
+                }
+            }
         }
         block.put(rollup_block_number).unwrap();
 
