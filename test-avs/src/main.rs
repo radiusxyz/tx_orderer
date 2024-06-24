cb93a7369ff8922e21faf7da3583acd872d95436
Kanet <zombiedelah@gmail.com>
2024-06-24 23:48:05 +0900

    WIP: respond_to_task()

--- test-avs/src/main.rs
+++ test-avs/src/main.rs
@@ -12,12 +12,12 @@ async fn main() -> Result<(), Error> {
         "http://127.0.0.1:8545",
         // "/home/kanet/Projects/sequencer-framework/sequencer/sequencer-avs/keys/sequencer_1",
         // "sequencer_1",
-        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
         "0x67d269191c92Caf3cD7723F116c85e6E9bf55933",
         "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
-        "0x9E545E3C0baAB3E08CdfD552C960A1050f373042",
+        "0xa82fF9aFd8f496c3d6ac40E2a0F282E47488CFc9",
         "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
-        "0x95401dc811bb5740090279Ba06cfA8fcF6113778",
+        "0x9E545E3C0baAB3E08CdfD552C960A1050f373042",
         "http://127.0.0.1:3000",
     )?;
 
@@ -26,7 +26,7 @@ async fn main() -> Result<(), Error> {
     let ssal_event_listener = SsalEventListener::connect(
         "ws://127.0.0.1:8545",
         "0x67d269191c92Caf3cD7723F116c85e6E9bf55933",
-        "0x95401dc811bb5740090279Ba06cfA8fcF6113778",
+        "0x9E545E3C0baAB3E08CdfD552C960A1050f373042",
     )
     .await?;
 
@@ -135,7 +135,7 @@ async fn callback(event: SsalEventType, context: SsalClient) {
                 .respond_to_task(
                     event.task,
                     event.taskIndex,
-                    "0x38a941d2d4959baae54ba9c14502abe54ffd4ad0db290295f453ef9d7d5a3f2d",
+                    "0x38a941d2d4959baae54ba9c14502abe54ffd4ad0db290295f453ef9d7d5a3f2d".to_owned(),
                 )
                 .await
             {
