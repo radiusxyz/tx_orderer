a97c7d5fb287967e3928949a7f55b51f2ee0dfc3
Kanet <zombiedelah@gmail.com>
2024-06-26 20:33:33 +0900

    WIP: Initial Rollup

--- sequencer/sequencer-avs/src/bin/test_rollup.rs
+++ sequencer/sequencer-avs/src/bin/test_rollup.rs
@@ -1,208 +1,171 @@
 use std::{env, time::Duration};
 
-use database::Database;
+use database::{database, Database};
 use json_rpc::RpcClient;
 use sequencer_avs::{
-    config::Config, error::Error, rpc::external::*, task::cluster_manager, types::*,
+    config::Config, error::Error, rpc::external::*, state::AppState, task::TraceExt, types::*,
 };
-use ssal::avs::{types::*, SsalClient};
+use ssal::avs::{types::*, SsalClient, SsalEventListener};
 use tokio::time::sleep;
+
+const SSAL_BLOCK_NUMBER_KEY: &'static str = "0";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt().init();
+    std::panic::set_hook(Box::new(|panic_info| tracing::error!("{}", panic_info)));
+
+    let arguments: Vec<String> = env::args().skip(1).collect();
+    let config_path = arguments
+        .get(0)
+        .expect("Provide the config file path.")
+        .to_owned();
+    let keystore_password = arguments
+        .get(1)
+        .expect("Provide the keystore password.")
+        .to_owned();
+    let block_margin: u64 = arguments
+        .get(2)
+        .expect("Provide the block margin.")
+        .parse()
+        .expect("Failed to parse the block margin argument to `u64`");
+    let block_creation_time: u64 = arguments
+        .get(3)
+        .expect("Provide the block creation time.")
+        .parse()
+        .expect("Failed to parse the block creation time argument to `u64`");
+
+    // Load the configuration from the path.
+    let config = Config::load(&config_path)?;
+    tracing::info!(
+        "Successfully loaded the configuration file at {}",
+        config_path,
+    );
+
+    // Initialize the database.
+    Database::new(config.database_path())?.init();
+    tracing::info!(
+        "Succesfully initialized the database at {:?}",
+        config.database_path(),
+    );
+
+    // Initialize the SSAL client.
+    let ssal_client = SsalClient::new(
+        config.ethereum_rpc_url(),
+        config.keystore_path(),
+        keystore_password,
+        config.seeder_rpc_url(),
+        config.ssal_contract_address(),
+        config.delegation_manager_contract_address(),
+        config.stake_registry_contract_address(),
+        config.avs_directory_contract_address(),
+        config.avs_contract_address(),
+    )?;
+    tracing::info!("Successfully initialized the SSAL client");
+
+    // Initialize an application-wide state instance.
+    let app_state = AppState::new(config, ssal_client, None);
+
+    // Initialize the event manager.
+    event_manager(app_state.clone());
+
+    // Load the database instance.
+    let database = database()?;
+
+    let previous_ssal_block_number = 0;
+    let mut rollup_block_number: u64 = 0;
+    loop {
+        let current_ssal_block_number = database
+            .get::<&'static str, u64>(&SSAL_BLOCK_NUMBER_KEY)
+            .ok_or_trace();
+
+        if let Some(ssal_block_number) = current_ssal_block_number {
+            let sequencer_list = SequencerList::get(ssal_block_number - block_margin).ok_or_trace();
+
+            if let Some(sequencer_list) = sequencer_list {
+                let leader_index = rollup_block_number
+                    .checked_rem(sequencer_list.len() as u64)
+                    .ok_or(Error::EmptySequencerList)
+                    .ok_or_trace();
+            }
+        }
+
+        sleep(Duration::from_secs(block_creation_time)).await;
+    }
+}
+
+fn event_manager(context: AppState) {
+    tokio::spawn(async move {
+        loop {
+            let ssal_event_listener = SsalEventListener::connect(
+                context.config().ethereum_websocket_url(),
+                context.config().ssal_contract_address(),
+                context.config().avs_contract_address(),
+            )
+            .await
+            .ok_or_trace();
+
+            if let Some(ssal_event_listener) = ssal_event_listener {
+                ssal_event_listener
+                    .init(event_callback, context.clone())
+                    .await
+                    .ok_or_trace();
+            }
+
+            sleep(Duration::from_secs(3)).await;
+            tracing::warn!("Reconnecting the event listener..");
+        }
+    });
+}
+
+async fn event_callback(event_type: SsalEventType, context: AppState) {
+    match event_type {
+        SsalEventType::NewBlock(block) => {
+            if let Some(block_number) = block.header.number {
+                let sequencer_list = context
+                    .ssal_client()
+                    .get_sequencer_list(context.config().cluster_id())
+                    .await
+                    .ok_or_trace();
+
+                if let Some(sequencer_list) = sequencer_list {
+                    SequencerList::from(sequencer_list)
+                        .put(block_number)
+                        .ok_or_trace();
+                }
+
+                database()
+                    .unwrap()
+                    .put(&SSAL_BLOCK_NUMBER_KEY, &block_number)
+                    .unwrap();
+            }
+        }
+        SsalEventType::BlockCommitment((event, _log)) => {
+            if &event.clusterID.to_string() == context.config().cluster_id() {
+                request_block(event.blockNumber).await.ok_or_trace();
+            }
+        }
+        _ => {}
+    }
+}
+
+async fn request_block(rollup_block_number: u64) -> Result<(), Error> {
+    let sequencer_list = SequencerList::get(rollup_block_number).ok_or_trace();
+
+    if let Some(sequencer_list) = sequencer_list {}
+
     Ok(())
 }
 
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//     tracing_subscriber::fmt().init();
+async fn request_build_block(block_margin: u64, rollup_block_number: u64) -> Result<(), Error> {
+    let ssal_block_number =
+        database()?.get::<&'static str, u64>(&SSAL_BLOCK_NUMBER_KEY)? - block_margin;
+    let sequencer_list = SequencerList::get(ssal_block_number)?;
 
-//     let arguments: Vec<String> = env::args().skip(1).collect();
-//     let config_path: String = arguments
-//         .get(0)
-//         .expect("Provide a configuration file path")
-//         .to_owned();
-//     let block_margin: u64 = arguments
-//         .get(1)
-//         .expect("Provide the block margin.")
-//         .parse()
-//         .expect("Failed to parse the block margin argument to `u64`");
-//     let block_creation_time: u64 = arguments
-//         .get(2)
-//         .expect("Provide the block creation time.")
-//         .parse()
-//         .expect("Failed to parse the block creation time argument to `u64`");
+    let rpc_method = BuildBlock {
+        ssal_block_number,
+        rollup_block_number,
+    };
 
-//     // Load the configuration from the path.
-//     let config = Config::load(config_path)?;
-
-//     // Initialize the database as a global singleton called by `database::database()`.
-//     Database::new(&config.database_path)?.init();
-
-//     // Initialize the SSAL client.
-//     let ssal_client = SsalClient::new(
-//         &config.ssal_rpc_address,
-//         &config.ssal_private_key,
-//         &config.contract_address,
-//         config.cluster_id,
-//         &config.seeder_rpc_address,
-//     )
-//     .await?;
-
-//     // Initialize the cluster manager.
-//     // cluster_manager::init(&ssal_client);
-
-//     // Initialize the rollup block number.
-//     let mut rollup_block_number = RollupBlockNumber::from(0);
-//     loop {
-//         if let Some(current_ssal_block_number) = SsalBlockNumber::get().ok() {
-//             let ssal_block_number = current_ssal_block_number - block_margin;
-//             if let Some(sequencer_list) = SequencerList::get(ssal_block_number).ok() {
-//                 tracing::info!("{:?}\n{:?}", ssal_block_number, rollup_block_number);
-//                 // tracing::info!("{:?}", sequencer_list);
-//                 let leader_index = rollup_block_number % sequencer_list.len();
-//                 let (leader, followers) = sequencer_list.split_leader_from_followers(leader_index);
-
-//                 match send_build_block(
-//                     ssal_block_number,
-//                     rollup_block_number,
-//                     &leader,
-//                     &followers,
-//                     3,
-//                     1,
-//                 )
-//                 .await
-//                 {
-//                     Ok(sequencer_status) => match sequencer_status {
-//                         SequencerStatus::Running => {
-//                             match send_get_block(rollup_block_number - 1, &leader, &followers, 3, 1)
-//                                 .await
-//                             {
-//                                 Ok(block) => tracing::info!("{:?}", block),
-//                                 Err(error) => tracing::error!("{}", error),
-//                             }
-//                             rollup_block_number += 1;
-//                         }
-//                         SequencerStatus::Uninitialized => rollup_block_number += 1,
-//                     },
-//                     Err(error) => tracing::error!("{}", error),
-//                 }
-//             }
-//         }
-//         sleep(Duration::from_secs(block_creation_time - 2)).await;
-//     }
-// }
-
-// async fn send_build_block(
-//     ssal_block_number: SsalBlockNumber,
-//     rollup_block_number: RollupBlockNumber,
-//     leader: &(H160, Option<String>),
-//     followers: &Vec<(H160, Option<String>)>,
-//     retry: usize,
-//     retry_interval: u64,
-// ) -> Result<(), Error> {
-//     let rpc_method = BuildBlock {
-//         ssal_block_number,
-//         rollup_block_number,
-//     };
-
-//     for retry_count in 0..retry {
-//         tracing::info!(
-//             "[{}] Trying the leader.. retry count: {}",
-//             stringify!(BuildBlock),
-//             retry_count,
-//         );
-//         if let Some(rpc_response) = send_to_leader(&leader, rpc_method.clone()).await.ok() {
-//             return Ok(rpc_response);
-//         }
-//         sleep(Duration::from_secs(retry_interval)).await;
-//     }
-
-//     for retry_count in 0..retry {
-//         tracing::info!(
-//             "[{}] Trying the followers.. retry count: {}",
-//             stringify!(BuildBlock),
-//             retry_count
-//         );
-//         if let Some(rpc_response) = send_to_followers(followers, rpc_method.clone()).await.ok() {
-//             return Ok(rpc_response);
-//         }
-//     }
-
-//     Err(Error::ClusterDown)
-// }
-
-// async fn send_get_block(
-//     rollup_block_number: RollupBlockNumber,
-//     leader: &(H160, Option<String>),
-//     followers: &Vec<(H160, Option<String>)>,
-//     retry: usize,
-//     retry_interval: u64,
-// ) -> Result<<GetBlock as RpcMethod>::Response, Error> {
-//     let rpc_method = GetBlock {
-//         rollup_block_number,
-//     };
-
-//     for retry_count in 0..retry {
-//         tracing::info!(
-//             "[{}] Trying the leader.. retry count: {}",
-//             stringify!(GetBlock),
-//             retry_count,
-//         );
-//         if let Some(rpc_response) = send_to_leader(&leader, rpc_method.clone()).await.ok() {
-//             return Ok(rpc_response);
-//         }
-//         sleep(Duration::from_secs(retry_interval)).await;
-//     }
-
-//     for retry_count in 0..retry {
-//         tracing::info!(
-//             "[{}] Trying the followers.. retry count: {}",
-//             stringify!(GetBlock),
-//             retry_count
-//         );
-//         if let Some(rpc_response) = send_to_followers(followers, rpc_method.clone()).await.ok() {
-//             return Ok(rpc_response);
-//         }
-//     }
-
-//     Err(Error::ClusterDown)
-// }
-
-// async fn send_to_leader<T>(
-//     leader: &(H160, Option<String>),
-//     rpc_method: T,
-// ) -> Result<<T as RpcMethod>::Response, Error>
-// where
-//     T: RpcMethod + Send,
-// {
-//     if let Some(rpc_address) = &leader.1 {
-//         let rpc_client = RpcClient::new(rpc_address, 2)?;
-//         let rpc_response = rpc_client.request(rpc_method).await?;
-//         Ok(rpc_response)
-//     } else {
-//         Err(Error::EmptyLeaderAddress)
-//     }
-// }
-
-// async fn send_to_followers<T>(
-//     followers: &Vec<(H160, Option<String>)>,
-//     rpc_method: T,
-// ) -> Result<<T as RpcMethod>::Response, Error>
-// where
-//     T: RpcMethod + Send,
-// {
-//     for follower in followers {
-//         if let Some(rpc_address) = &follower.1 {
-//             let rpc_client = RpcClient::new(rpc_address, 2)?;
-//             match rpc_client.request(rpc_method.clone()).await {
-//                 Ok(rpc_response) => return Ok(rpc_response),
-//                 Err(_) => continue,
-//             }
-//         } else {
-//             continue;
-//         }
-//     }
-
-//     Err(Error::UnresponsiveFollowers)
-// }
+    Ok(())
+}
