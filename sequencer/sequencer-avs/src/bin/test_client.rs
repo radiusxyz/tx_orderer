ead88906099f931407532c50460f72350a55aa3c
Kanet <zombiedelah@gmail.com>
2024-06-26 19:45:25 +0900

    Initial test client

--- sequencer/sequencer-avs/src/bin/test_client.rs
+++ sequencer/sequencer-avs/src/bin/test_client.rs
@@ -1,67 +1,105 @@
 use std::env;
 
-use database::Database;
+use database::{database, Database};
 use json_rpc::RpcClient;
+use rand::{rngs::ThreadRng, seq::SliceRandom};
 use sequencer_avs::{
-    config::Config, error::Error, rpc::external::SendTransaction, task::cluster_manager, types::*,
+    config::Config, error::Error, rpc::external::SendTransaction, state::AppState, task::TraceExt,
+    types::*,
 };
-use ssal::avs::SsalClient;
+use ssal::avs::{types::SsalEventType, SsalClient, SsalEventListener};
 use tokio::time::{sleep, Duration};
+
+const SSAL_BLOCK_NUMBER_KEY: &'static str = "0";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    Ok(())
+    tracing_subscriber::fmt().init();
+    std::panic::set_hook(Box::new(|panic_info| tracing::error!("{}", panic_info)));
 
-    // tracing_subscriber::fmt().init();
+    let arguments: Vec<String> = env::args().skip(1).collect();
+    let config_path = arguments
+        .get(0)
+        .expect("Provide the config file path.")
+        .to_owned();
+    let keystore_password = arguments
+        .get(1)
+        .expect("Provide the keystore password.")
+        .to_owned();
+    let block_margin: u64 = arguments
+        .get(2)
+        .expect("Provide the block margin.")
+        .parse()
+        .expect("Failed to parse the block margin argument to `u64`");
 
-    // let arguments: Vec<String> = env::args().skip(1).collect();
-    // let config_path = arguments
-    //     .get(0)
-    //     .expect("Provide the config file path.")
-    //     .to_owned();
-    // let block_margin: u64 = arguments
-    //     .get(1)
-    //     .expect("Provide the block margin.")
-    //     .parse()
-    //     .expect("Failed to parse the block margin argument to `u64`");
+    // Load the configuration from the path.
+    let config = Config::load(&config_path)?;
+    tracing::info!(
+        "Successfully loaded the configuration file at {}",
+        config_path,
+    );
 
-    // // Load the configuration from the path.
-    // let config = Config::load(config_path)?;
+    // Initialize the database.
+    Database::new(config.database_path())?.init();
+    tracing::info!(
+        "Succesfully initialized the database at {:?}",
+        config.database_path(),
+    );
 
-    // // Initialize the database as a global singleton called by `database::database()`.
-    // Database::new(&config.database_path)?.init();
+    // Initialize the SSAL client.
+    let ssal_client = SsalClient::new(
+        config.ethereum_rpc_url(),
+        config.keystore_path(),
+        keystore_password,
+        config.seeder_rpc_url(),
+        config.ssal_contract_address(),
+        config.delegation_manager_contract_address(),
+        config.stake_registry_contract_address(),
+        config.avs_directory_contract_address(),
+        config.avs_contract_address(),
+    )?;
+    tracing::info!("Successfully initialized the SSAL client");
 
-    // // Initialize the SSAL client.
-    // let ssal_client = SsalClient::new(
-    //     &config.ssal_rpc_address,
-    //     &config.ssal_private_key,
-    //     &config.contract_address,
-    //     config.cluster_id,
-    //     &config.seeder_rpc_address,
-    // )
-    // .await?;
+    // Initialize an application-wide state instance.
+    let app_state = AppState::new(config, ssal_client, None);
 
-    // // Initialize the cluster manager.
-    // // cluster_manager::init(&ssal_client);
+    // Initialize the event manager.
+    event_manager(app_state.clone());
 
-    // // Start sending the transaction.
-    // loop {
-    //     if let Ok(current_ssal_block_number) = SsalBlockNumber::get() {
-    //         if let Ok(sequencer_list) = SequencerList::get(current_ssal_block_number - block_margin)
-    //         {
-    //             transaction_sender(sequencer_list).await;
-    //         }
-    //     }
-    // }
+    // Initialize the random seed.
+    let mut seed = rand::thread_rng();
+
+    // Load the database instance.
+    let database = database()?;
+
+    // Start sending the transaction.
+    loop {
+        let current_ssal_block_number = database
+            .get::<&'static str, u64>(&SSAL_BLOCK_NUMBER_KEY)
+            .ok_or_trace();
+
+        if let Some(ssal_block_number) = current_ssal_block_number {
+            let sequencer_list = SequencerList::get(ssal_block_number - block_margin).ok_or_trace();
+            if let Some(sequencer_list) = sequencer_list {
+                send_transaction(sequencer_list, &mut seed)
+                    .await
+                    .ok_or_trace();
+            }
+        }
+
+        sleep(Duration::from_millis(200)).await;
+    }
 }
 
-async fn transaction_sender(sequencer_list: SequencerList) {
-    for (_public_key, rpc_address) in sequencer_list.iter() {
-        if let Some(rpc_address) = rpc_address {
-            let rpc_address = rpc_address.clone();
-
-            tokio::spawn(async move {
-                let transaction = UserTransaction::new(
+async fn send_transaction(
+    sequencer_list: SequencerList,
+    seed: &mut ThreadRng,
+) -> Result<(), Error> {
+    let sequencer_list = sequencer_list.into_inner();
+    let sequencer = sequencer_list.choose(seed);
+    if let Some((_address, rpc_url)) = sequencer {
+        if let Some(rpc_url) = rpc_url {
+            let transaction = UserTransaction::new(
                     EncryptedTransaction::new("04ef55bd572cf13ee6e71e6fc99f4c81f651956fade173ae258d2146b451456565b198527a90fbe9ed82e79b287b8825b49590cc79f6357c0bdd04b8797829712adbecf768d946a083e17775bde91fc0cd16d863dd82598de10e31bfab47102a561d8c5950193c39177691a3c80e85a2c414c1f0f8115afd8b60855ccb1be61c2a3a5691cde8757cd8ede1741dd1e803184695552e7efe78d1b6c03b811f772f24ffc9c2eebf2a66b4b49f4c47c86aa3fbb6631de0dd05c70f058a17fd92df56901eb15b753b2c9863a200692d0b03322ae1fb8a5cdb047c23ae048d65e8c972f70a979e6bb514445a11a86d05f0db6933f7331ec70c0f4c6f1378473722925821395a320f7d110d3714ed82bd586791995c6583b8637b6d54ac64ada0409c503e9f8981f78a84121af279a494394094e4a9682fa6b30237b76f3f4a05687b374e9cee2ff806f55d97a288546e58f11370300d10605e9706659ee65c0d5a824c1fe732c14b345cff07a5226d75781f1596210b878543e9fdfdc0518cb11cb460ada14165453571d14fa4a60bc79e93270f3e397322bdeb8a731723a00dfab35f3e0c6b66d2483628a56a966843bbd653cb1bd1ac9154ee2b6d290fa6ac49525caaadd9ba352437a76be70aee825c4358cdc9b2eadf32c203b486b2cb5393d73f504a02a47019072e96b4a2ae8a906865def86d214b3a305ba44d6522150c5868"),
                     TimeLockPuzzle::new(
                         22,
@@ -71,16 +109,65 @@ async fn transaction_sender(sequencer_list: SequencerList) {
                     Nonce::new("e90167be0f9daa0be9c9ca4b1716f9541442eccc4b7fbe9224a2b6212f229b16"),
                 );
 
-                let rpc_client = RpcClient::new(rpc_address.clone(), 3).unwrap();
-                let rpc_method = SendTransaction { transaction };
-                let response: OrderCommitment = rpc_client
-                    .request(SendTransaction::METHOD_NAME, rpc_method)
-                    .await
-                    .unwrap();
-                tracing::info!("{:?}", response);
-            });
+            let rpc_client = RpcClient::new(rpc_url)?;
+            let rpc_method = SendTransaction { transaction };
+            let order_commitment: OrderCommitment = rpc_client
+                .request(SendTransaction::METHOD_NAME, rpc_method)
+                .await?;
 
-            sleep(Duration::from_millis(500)).await;
+            tracing::info!("{:?}", order_commitment);
         }
+    }
+
+    Ok(())
+}
+
+fn event_manager(context: AppState) {
+    tokio::spawn(async move {
+        loop {
+            let ssal_event_listener = SsalEventListener::connect(
+                context.config().ethereum_websocket_url(),
+                context.config().ssal_contract_address(),
+                context.config().avs_contract_address(),
+            )
+            .await
+            .ok_or_trace();
+
+            if let Some(ssal_event_listener) = ssal_event_listener {
+                ssal_event_listener
+                    .init(event_callback, context.clone())
+                    .await
+                    .ok_or_trace();
+            }
+
+            sleep(Duration::from_secs(3)).await;
+            tracing::warn!("Reconnecting the event listener..");
+        }
+    });
+}
+
+async fn event_callback(event_type: SsalEventType, context: AppState) {
+    match event_type {
+        SsalEventType::NewBlock(block) => {
+            if let Some(block_number) = block.header.number {
+                let sequencer_list = context
+                    .ssal_client()
+                    .get_sequencer_list(context.config().cluster_id())
+                    .await
+                    .ok_or_trace();
+
+                if let Some(sequencer_list) = sequencer_list {
+                    SequencerList::from(sequencer_list)
+                        .put(block_number)
+                        .ok_or_trace();
+                }
+
+                database()
+                    .unwrap()
+                    .put(&SSAL_BLOCK_NUMBER_KEY, &block_number)
+                    .unwrap();
+            }
+        }
+        _ => {}
     }
 }
