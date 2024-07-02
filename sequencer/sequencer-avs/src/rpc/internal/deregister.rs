367f33105589def163d66c800a8ad85749da5c7b
Kanet <zombiedelah@gmail.com>
2024-07-02 12:03:14 +0900

    Add 'deregister_operator()'

--- sequencer/sequencer-avs/src/rpc/internal/deregister.rs
+++ sequencer/sequencer-avs/src/rpc/internal/deregister.rs
@@ -12,6 +12,8 @@ impl Deregister {
             .deregister_sequencer(context.config().cluster_id())
             .await?;
 
+        context.ssal_client().deregister_operator().await?;
+
         shutdown::init(context.ssal_client());
 
         Ok(())
