2606c8c5c3f6ee44f93f8d9f8f722848f4fe4092
kanet1105 <zombiedelah@gmail.com>
2024-07-03 20:58:17 +0900

    Print logs if and only if the health checking succeeds

--- seeder/seeder-avs/src/rpc/deregister.rs
+++ seeder/seeder-avs/src/rpc/deregister.rs
@@ -2,9 +2,10 @@ use super::prelude::*;
 
 pub async fn handler(parameter: RpcParameter, _context: Arc<()>) -> Result<(), RpcError> {
     let parameter = parameter.parse::<Deregister>()?;
+
+    database()?.delete(&parameter.sequencer_address)?;
+
     tracing::info!("{:?}", parameter);
 
-    database()?
-        .delete(&parameter.sequencer_address)
-        .map_err(|error| error.into())
+    Ok(())
 }
--- seeder/seeder-avs/src/rpc/register.rs
+++ seeder/seeder-avs/src/rpc/register.rs
@@ -2,11 +2,12 @@ use super::prelude::*;
 
 pub async fn handler(parameter: RpcParameter, _context: Arc<()>) -> Result<(), RpcError> {
     let parameter = parameter.parse::<Register>()?;
-    tracing::info!("{:?}", parameter);
 
     health_check(&parameter.sequencer_rpc_url).await?;
 
-    database()?
-        .put(&parameter.sequencer_address, &parameter.sequencer_rpc_url)
-        .map_err(|error| error.into())
+    database()?.put(&parameter.sequencer_address, &parameter.sequencer_rpc_url)?;
+
+    tracing::info!("{:?}", parameter);
+
+    Ok(())
 }
