fd07e67c39b60320af7e1a3f1de6c1e1bdccdc2a
jaemin <datactress@gmail.com>
2024-08-01 15:33:44 +0900

    chore: Add zkp methods

--- sequencer/src/types/zkp.rs
+++ sequencer/src/types/zkp.rs
@@ -4,8 +4,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct EncryptionProof(Vec<u8>);
 
+impl EncryptionProof {
+    pub fn new(value: Vec<u8>) -> Self {
+        Self(value)
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct TimeLockPuzzleProof(Vec<u8>);
+
+impl TimeLockPuzzleProof {
+    pub fn new(value: Vec<u8>) -> Self {
+        Self(value)
+    }
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct PvdePublicInput {
@@ -17,9 +29,43 @@ pub struct PvdePublicInput {
     k_hash_value: PoseidonHashValue,
 }
 
+impl PvdePublicInput {
+    pub fn new(
+        r1: BigUint,
+        r2: BigUint,
+        z: BigUint,
+        o: BigUint,
+        k_two: BigUint,
+        k_hash_value: PoseidonHashValue,
+    ) -> Self {
+        Self {
+            r1,
+            r2,
+            z,
+            o,
+            k_two,
+            k_hash_value,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct PvdeZkp {
     public_input: PvdePublicInput,
     time_lock_puzzle_proof: TimeLockPuzzleProof,
     encryption_proof: EncryptionProof,
+}
+
+impl PvdeZkp {
+    pub fn new(
+        public_input: PvdePublicInput,
+        time_lock_puzzle_proof: TimeLockPuzzleProof,
+        encryption_proof: EncryptionProof,
+    ) -> Self {
+        Self {
+            public_input,
+            time_lock_puzzle_proof,
+            encryption_proof,
+        }
+    }
 }
