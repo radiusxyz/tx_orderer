ff1d57314414f1f871bb85b3976e6b21a0b39b01
Kanet <zombiedelah@gmail.com>
2024-08-30 18:00:49 +0900

    Move all getter RPCs under 'debug' module

---
+++ src/rpc/debug/get_cluster_id_list.rs
@@ -0,0 +1,33 @@
+use crate::{models::ClusterIdListModel, rpc::prelude::*};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetClusterIdList {
+    platform: PlatForm,
+    sequencing_function_type: SequencingFunctionType,
+    service_type: ServiceType,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetClusterIdListResponse {
+    cluster_id_list: ClusterIdList,
+}
+
+impl GetClusterIdList {
+    pub const METHOD_NAME: &'static str = "get_cluster_id_list";
+
+    pub async fn handler(
+        parameter: RpcParameter,
+        _context: Arc<AppState>,
+    ) -> Result<GetClusterIdListResponse, RpcError> {
+        let parameter = parameter.parse::<Self>()?;
+
+        let cluster_id_list = ClusterIdListModel::get(
+            &parameter.platform,
+            &parameter.sequencing_function_type,
+            &parameter.service_type,
+        )?
+        .cluster_id_list();
+
+        Ok(GetClusterIdListResponse { cluster_id_list })
+    }
+}
