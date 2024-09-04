29b01946fa3e3c70179d27fc1256ce650681938d
kanet <zombiedelah@gmail.com>
2024-09-04 16:20:25 +0900

    Refactored

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,7 +1,2 @@
-mod liveness_client;
-mod seeder_client;
-mod sequencer_client;
-
-pub use liveness_client::*;
-pub use seeder_client::*;
-pub use sequencer_client::*;
+pub mod liveness;
+pub mod validation;
