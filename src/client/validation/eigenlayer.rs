710e7aebe16c3a5ba2fb4ee18c47310322f3be65
kanet <zombiedelah@gmail.com>
2024-09-04 21:14:41 +0900

    Initiali 'JoinCluster'

---
+++ src/client/validation/eigenlayer.rs
@@ -0,0 +1,65 @@
+use std::sync::Arc;
+
+use radius_sequencer_sdk::validation_eigenlayer::{
+    publisher::Publisher, subscriber::Subscriber, types::Avs,
+};
+use tokio::time::{sleep, Duration};
+
+use crate::error::Error;
+
+pub struct ValidationClient {
+    inner: Arc<ValidationClientInner>,
+}
+
+struct ValidationClientInner {
+    publisher: Publisher,
+    subscriber: Subscriber,
+}
+
+unsafe impl Send for ValidationClient {}
+
+unsafe impl Sync for ValidationClient {}
+
+impl Clone for ValidationClient {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl ValidationClient {
+    pub fn new() -> Result<Self, Error> {
+        todo!("Contract refactoring");
+        // let publisher = Publisher::new();
+    }
+
+    pub fn publisher(&self) -> &Publisher {
+        &self.inner.publisher
+    }
+
+    pub fn subscriber(&self) -> &Subscriber {
+        &self.inner.subscriber
+    }
+
+    pub fn initialize_event_listener(&self) {
+        let validation_client = self.clone();
+
+        tokio::spawn(async move {
+            loop {
+                validation_client
+                    .subscriber()
+                    .initialize_event_handler(callback, validation_client.clone())
+                    .await
+                    .unwrap();
+
+                tracing::warn!("Reconnecting the eigenlayer validation event listener..");
+                sleep(Duration::from_secs(5)).await;
+            }
+        });
+    }
+}
+
+async fn callback(event: Avs::NewTaskCreated, context: ValidationClient) {
+    // Todo: Handle block commitment events.
+}
--- src/rpc/internal/join_cluster.rs
+++ src/rpc/internal/join_cluster.rs
@@ -1,14 +1,10 @@
-use crate::{
-    models::{ClusterIdListModel, LivenessClusterModel, ValidationClusterModel},
-    rpc::prelude::*,
-    util::initialize_liveness_cluster,
-};
+use crate::rpc::prelude::*;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct JoinCluster {
     platform: Platform,
     service_provider: ServiceProvider,
-    cluster_id: ClusterId,
+    cluster_id: String,
 }
 
 impl JoinCluster {
@@ -17,91 +13,10 @@ impl JoinCluster {
     pub async fn handler(parameter: RpcParameter, context: Arc<AppState>) -> Result<(), RpcError> {
         let parameter = parameter.parse::<Self>()?;
 
-        // TODO (1): Add the cluster ID to the listener task if it exists. Otherwise, spawn a new listener task.
-
-        // match parameter.sequencing_function_type {
-        //     SequencingFunctionType::Liveness => {
-        //         match LivenessClusterModel::get(
-        //             &parameter.platform,
-        //             &parameter.service_type,
-        //             &parameter.cluster_id,
-        //         ) {
-        //             Ok(_) => {}
-        //             Err(_) => {
-        //                 let cluster_model = LivenessClusterModel::new(
-        //                     parameter.platform.clone(),
-        //                     parameter.service_type.clone(),
-        //                     parameter.cluster_id.clone(),
-        //                 );
-
-        //                 cluster_model.put()?;
-        //             }
-        //         }
-        //     }
-        //     SequencingFunctionType::Validation => {
-        //         match ValidationClusterModel::get(
-        //             &parameter.platform,
-        //             &parameter.service_type,
-        //             &parameter.cluster_id,
-        //         ) {
-        //             Ok(_) => {}
-        //             Err(_) => {
-        //                 let cluster_model = ValidationClusterModel::new(
-        //                     parameter.platform.clone(),
-        //                     parameter.service_type.clone(),
-        //                     parameter.cluster_id.clone(),
-        //                 );
-
-        //                 cluster_model.put()?;
-        //             }
-        //         }
-        //     }
-        // }
-
-        // // TODO: get operator information
-        // match ClusterIdListModel::get_mut(
-        //     &parameter.platform,
-        //     &parameter.sequencing_function_type,
-        //     &parameter.service_type,
-        // ) {
-        //     Ok(mut cluster_id_list) => {
-        //         cluster_id_list.add_cluster_id(parameter.cluster_id.clone());
-        //         cluster_id_list.update()?;
-        //     }
-        //     Err(err) => {
-        //         if err.is_none_type() {
-        //             let mut cluster_id_list_model = ClusterIdListModel::default();
-        //             cluster_id_list_model.add_cluster_id(parameter.cluster_id.clone());
-        //             cluster_id_list_model.put(
-        //                 &parameter.platform,
-        //                 &parameter.sequencing_function_type,
-        //                 &parameter.service_type,
-        //             )?;
-        //         } else {
-        //             return Err(err.into());
-        //         }
-        //     }
-        // };
-
-        // let signing_key = context.signing_key();
-        // let seeder_client = context.seeder_client();
-        // let sequencing_info_key = SequencingInfoKey::new(
-        //     parameter.platform.clone(),
-        //     parameter.sequencing_function_type.clone(),
-        //     parameter.service_type.clone(),
-        // );
-
-        // let sequencing_info = context.get_sequencing_info(&sequencing_info_key)?;
-
-        // let cluster = initialize_liveness_cluster(
-        //     signing_key,
-        //     &seeder_client,
-        //     &sequencing_info,
-        //     &parameter.cluster_id,
-        // )
-        // .await?;
-
-        // context.set_cluster(cluster);
+        let mut cluster_id_list =
+            ClusterIdListModel::get_mut(parameter.platform, parameter.service_provider)?;
+        cluster_id_list.insert(&parameter.cluster_id);
+        cluster_id_list.update()?;
 
         Ok(())
     }
