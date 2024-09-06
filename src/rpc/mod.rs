24801de4027ec4f98d2ec2aaf97ec35aa1a06128
Kanet <zombiedelah@gmail.com>
2024-09-07 00:28:41 +0900

    internal

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -4,7 +4,7 @@ pub mod internal;
 pub(crate) mod prelude {
     pub use std::sync::Arc;
 
-    pub use radius_sequencer_sdk::json_rpc::{types::*, RpcError};
+    pub use radius_sequencer_sdk::json_rpc::{types::*, RpcClient, RpcError};
     pub use serde::{Deserialize, Serialize};
 
     pub use crate::{client::liveness, error::Error, state::AppState, task::*, types::*};
