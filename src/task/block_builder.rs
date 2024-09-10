5b672f8ed10d68831cae57af2102a59d8a72254b
Kanet <zombiedelah@gmail.com>
2024-09-10 16:55:12 +0900

    WIP: Implement the builder

--- src/task/block_builder.rs
+++ src/task/block_builder.rs
@@ -1,16 +1,5 @@
 use crate::types::*;
 
-pub fn block_builder(rollup_id: &String, rollup_metadata: RollupMetadata) {
-    let rollup_id = rollup_id.clone();
-    let block_height = rollup_metadata.block_height();
-    let transaction_order = rollup_metadata.transaction_order();
-
-    tokio::spawn(async move {
-        for order_index in 0..transaction_order {
-            let encrypted_transaction =
-                EncryptedTransactionModel::get(&rollup_id, block_height, order_index).unwrap();
-        }
-    });
+pub fn block_builder(_rollup_id: &String, _rollup_metadata: RollupMetadata) {
+    todo!("Implement the block builder");
 }
-
-pub fn fetch_missing_transaction(transaction_order: u64) {}
