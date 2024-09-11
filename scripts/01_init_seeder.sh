32af1749345e4ff44f92ac918d6b7c13b02cc24d
stompesi <stompesi@gmail.com>
2024-09-11 14:48:30 +0900

    Remove no need files

--- scripts/01_init_seeder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source $SCRIPT_PATH/env.sh
-
-DATA_PATH=$CURRENT_PATH/seeder
-
-rm -rf $DATA_PATH
-
-$SEEDER_BIN_PATH init --path $DATA_PATH
