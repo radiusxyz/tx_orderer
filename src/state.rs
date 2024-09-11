0acdcd5e7ab868f9967f3036af2dd36bf012c74f
stompesi <stompesi@gmail.com>
2024-09-11 15:11:22 +0900

    Add Arcinner to Appstate

--- src/state.rs
+++ src/state.rs
@@ -1,23 +1,43 @@
+use std::sync::Arc;
+
 use crate::{client::liveness::seeder::SeederClient, types::*};
 
 pub struct AppState {
+    inner: Arc<AppStateInner>,
+}
+struct AppStateInner {
     config: Config,
     seeder_client: SeederClient,
 }
 
+unsafe impl Send for AppState {}
+unsafe impl Sync for AppState {}
+
+impl Clone for AppState {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
 impl AppState {
     pub fn new(config: Config, seeder_client: SeederClient) -> Self {
-        Self {
+        let inner = AppStateInner {
             config,
             seeder_client,
+        };
+
+        Self {
+            inner: Arc::new(inner),
         }
     }
 
     pub fn config(&self) -> &Config {
-        &self.config
+        &self.inner.config
     }
 
     pub fn seeder_client(&self) -> &SeederClient {
-        &self.seeder_client
+        &self.inner.seeder_client
     }
 }
