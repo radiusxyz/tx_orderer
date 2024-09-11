b1fe768051f7041779b02f4159cbad3df41d894f
stompesi <stompesi@gmail.com>
2024-09-11 15:31:02 +0900

    Add initialize sequencing info (in sequencer)

--- src/bin/sequencer.rs
+++ src/bin/sequencer.rs
@@ -26,7 +26,7 @@ use pvde::{
 };
 use radius_sequencer_sdk::{json_rpc::RpcServer, kvstore::KvStore as Database};
 use sequencer::{
-    client::liveness::seeder::SeederClient,
+    client::liveness::{self, seeder::SeederClient},
     error::{self, Error},
     rpc::{cluster, external, internal},
     state::AppState,
@@ -34,6 +34,7 @@ use sequencer::{
 };
 pub use serde::{Deserialize, Serialize};
 use tokio::task::JoinHandle;
+use tracing::info;
 
 #[derive(Debug, Deserialize, Parser, Serialize)]
 #[command(author, version, about, long_about = None)]
@@ -92,17 +93,6 @@ async fn main() -> Result<(), Error> {
                 config.database_path(),
             );
 
-            // Initialize sequencing info
-            // let sequencing_info_list = SequencingInfoListModel::get_or_default();
-            // sequencing_info_list.iter().for_each(|sequencing_info| {
-            //     info!(
-            //         "platform: {:?}, sequencing_function_type: {:?}, service_type: {:?}",
-            //         sequencing_info.platform(),
-            //         sequencing_info.sequencing_function_type(),
-            //         sequencing_info.service_type()
-            //     );
-            // });
-
             // Initialize seeder client
             let seeder_rpc_url = config.seeder_rpc_url();
             let seeder_client = SeederClient::new(seeder_rpc_url)?;
@@ -111,37 +101,36 @@ async fn main() -> Result<(), Error> {
                 seeder_rpc_url,
             );
 
-            // get or init rollup id list model
-            // let rollup_id_list_model = match RollupIdListModel::get() {
-            //     Ok(rollup_id_list_model) => rollup_id_list_model,
-            //     Err(err) => {
-            //         if err.is_none_type() {
-            //             let new_rollup_id_list_model = RollupIdListModel::default();
-            //             new_rollup_id_list_model.put()?;
-            //             new_rollup_id_list_model
-            //         } else {
-            //             return Err(err.into());
-            //         }
-            //     }
-            // };
+            // Initialize sequencing info (TODO:)
+            let sequencing_info_list =
+                SequencingInfoListModel::get_or_default().map_err(error::Error::Database)?;
+            for ((platform, service_provider), sequencing_info_payload) in
+                sequencing_info_list.iter()
+            {
+                info!(
+                    "platform: {:?}, service_provider: {:?}",
+                    platform, service_provider
+                );
 
-            // let rollup_id_list = rollup_id_list_model.rollup_id_list();
+                // TODO: register sequencer rpc url
 
-            // let rollup_states = rollup_id_list
-            //     .iter()
-            //     .map(|rollup_id| -> Result<(RollupId, RollupState), Error> {
-            //         let rollup_model = RollupModel::get(rollup_id)?;
-            //         let cluster_id = rollup_model.cluster_id().clone();
-            //         let rollup_block_height = RollupMetadataModel::get(rollup_id)?
-            //             .rollup_metadata()
-            //             .block_height();
-
-            //         Ok((
-            //             rollup_id.clone(),
-            //             RollupState::new(cluster_id, rollup_block_height),
-            //         ))
-            //     })
-            //     .collect::<Result<HashMap<RollupId, RollupState>, Error>>()?;
+                match &sequencing_info_payload {
+                    SequencingInfoPayload::Ethereum(liveness_info) => {
+                        liveness::radius::LivenessClient::new(
+                            platform.clone(),
+                            service_provider.clone(),
+                            liveness_info.clone(),
+                            config.signing_key(),
+                            seeder_client.clone(),
+                        )?
+                        .initialize_event_listener();
+                    }
+                    SequencingInfoPayload::Local(_payload) => {
+                        // liveness::local::LivenessClient::new()?;
+                        todo!("Implement 'LivenessClient' for local sequencing.");
+                    }
+                }
+            }
 
             let pvde_params = if let Some(ref path) = config_option.path {
                 // Initialize the time lock puzzle parameters.
@@ -150,89 +139,14 @@ async fn main() -> Result<(), Error> {
                 None
             };
 
-            // get or init sequencing infos, can be loaded after restarting
-            // let sequencing_info_model = match SequencingInfoModel::get() {
-            //     Ok(sequencing_info_model) => sequencing_info_model,
-            //     Err(err) => {
-            //         if err.is_none_type() {
-            //             SequencingInfoModel::new(HashMap::new())
-            //         } else {
-            //             return Err(err.into());
-            //         }
-            //     }
-            // };
-            // let sequencing_infos = sequencing_info_model.sequencing_infos();
-
             // Initialize an application-wide state instance
             let app_state = AppState::new(config, seeder_client);
-
-            // Add listener for each sequencing info
-            // sequencing_infos.iter().for_each(
-            //     |(sequencing_info_key, sequencing_info)| {
-            //         info!(
-            //             "platform: {:?}, sequencing_function_type: {:?}, service_type:
-            // {:?}",             sequencing_info_key.platform(),
-            // sequencing_info_key.sequencing_function_type(),
-            // sequencing_info_key.service_type()         );
-
-            //         match sequencing_info_key.platform() {
-            //             Platform::Local => {
-            //                 // TODO:
-            //                 info!("Init local platform (TODO)");
-            //             }
-            //             Platform::Ethereum => match
-            // sequencing_info_key.sequencing_function_type() {
-            // sequencer::types::SequencingFunctionType::Liveness => {
-            //                     match sequencing_info_key.service_type() {
-            //                         ServiceType::Radius => {
-            //                             info!(
-            //                                 "Init radius liveness -
-            // provider_websocket_url: {:?}",
-            // sequencing_info.provider_websocket_url
-            // );
-
-            //                             let sync_info = SyncInfo::new(
-            //                                 sequencing_info.clone(),
-            //                                 Arc::new(app_state.clone()),
-            //                             );
-            //                             radius_liveness_event_listener::init(
-            //                                 Arc::new(sync_info),
-            //                             );
-            //                         }
-            //                         _ => {
-            //                             // TODO:
-            //                             info!(
-            //                                 "Init other liveness (TODO) -
-            // provider_websocket_url: {:?}",
-            // sequencing_info.provider_websocket_url
-            // );                         }
-            //                     }
-            //                 }
-            //                 sequencer::types::SequencingFunctionType::Validation => {}
-            //             },
-            //         }
-            //     },
-            // );
 
             // Initialize the internal RPC server
             initialize_internal_rpc_server(&app_state).await?;
 
             // Initialize the cluster RPC server
             initialize_cluster_rpc_server(&app_state).await?;
-
-            // Initialize clusters
-            initialize_clusters(&app_state).await?;
-
-            // SKDE
-            // for rollup_id in rollup_id_list.iter() {
-            //     let rollup_model = RollupModel::get(rollup_id).unwrap();
-            //     let cluster_id = rollup_model.cluster_id().clone();
-
-            //     let cluster = app_state.get_cluster(&cluster_id).await.unwrap();
-
-            //     // TODO: only skde
-            //     init_single_key_generator(rollup_id.clone(), cluster);
-            // }
 
             // Initialize the external RPC server.
             let server_handle = initialize_external_rpc_server(&app_state).await?;
@@ -244,149 +158,60 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn initialize_clusters(app_state: &AppState) -> Result<(), Error> {
-    // let config = app_state.config();
-    // let seeder_client = app_state.seeder_client();
-    // let signing_key = config.signing_key();
+async fn initialize_internal_rpc_server(context: &AppState) -> Result<(), Error> {
+    let internal_rpc_url = context.config().internal_rpc_url().to_string();
 
-    // let address = config.address();
+    // Initialize the internal RPC server.
+    let internal_rpc_server = RpcServer::new(context.clone())
+        .init(internal_rpc_url.clone())
+        .await?;
 
-    // // The cluster rpc url is the rpc url of the sequencer
-    // let cluster_rpc_url = config.cluster_rpc_url().to_string();
+    tracing::info!(
+        "Successfully started the internal RPC server: {}",
+        internal_rpc_url
+    );
 
-    // // Register sequencer rpc url (with cluster rpc url) to seeder
-    // match seeder_client.get_rpc_url(&address).await {
-    //     Ok(rpc_url) => {
-    //         if rpc_url != cluster_rpc_url {
-    //             // TODO: Check
-    //             seeder_client
-    //                 .register_rpc_url(address.clone(),
-    // cluster_rpc_url.to_string())                 .await?;
-    //         }
-    //     }
-    //     Err(_) => {
-    //         seeder_client
-    //             .register_rpc_url(address.clone(), cluster_rpc_url.to_string())
-    //             .await?;
-    //     }
-    // }
-
-    // // get or init sequencing infos, can be loaded after restarting
-    // let sequencing_info_model = match SequencingInfoModel::get() {
-    //     Ok(sequencing_info_model) => sequencing_info_model,
-    //     Err(err) => {
-    //         if err.is_none_type() {
-    //             SequencingInfoModel::default()
-    //         } else {
-    //             return Err(err.into());
-    //         }
-    //     }
-    // };
-
-    // for (sequencing_info_key, sequencing_info) in
-    // sequencing_info_model.sequencing_infos().iter() {     info!(
-    //         "platform: {:?}, sequencing_function_type: {:?}, service_type: {:?}",
-    //         sequencing_info_key.platform(),
-    //         sequencing_info_key.sequencing_function_type(),
-    //         sequencing_info_key.service_type()
-    //     );
-
-    //     // Get all cluster ids for each sequencing info
-    //     let cluster_id_list_model = match ClusterIdListModel::get(
-    //         sequencing_info_key.platform(),
-    //         sequencing_info_key.sequencing_function_type(),
-    //         sequencing_info_key.service_type(),
-    //     ) {
-    //         Ok(cluster_id_list_model) => cluster_id_list_model,
-    //         Err(err) => {
-    //             if err.is_none_type() {
-    //                 continue;
-    //             } else {
-    //                 return Err(err.into());
-    //             }
-    //         }
-    //     };
-
-    //     // Initialize each cluster
-    //     for cluster_id in cluster_id_list_model.cluster_id_list().iter() {
-    //         info!("initialize_cluster: {:?}", cluster_id);
-
-    //         match sequencing_info_key.sequencing_function_type() {
-    //             SequencingFunctionType::Liveness => {
-    //                 let cluster = initialize_liveness_cluster(
-    //                     &SigningKey::from(signing_key.clone()),
-    //                     &seeder_client,
-    //                     sequencing_info,
-    //                     cluster_id,
-    //                 )
-    //                 .await?;
-
-    //                 app_state.set_cluster(cluster);
-    //             }
-    //             SequencingFunctionType::Validation => {
-    //                 // TODO:
-    //             }
-    //         }
-    //     }
-    // }
-    Ok(())
-}
-
-async fn initialize_internal_rpc_server(app_state: &AppState) -> Result<(), Error> {
-    // let internal_rpc_url = app_state.config().internal_rpc_url().to_string();
-
-    // // Initialize the internal RPC server.
-    // let internal_rpc_server = RpcServer::new(app_state)
-    //     // Todo: implement
-    //     .init(app_state.config().internal_rpc_url().to_string())
-    //     .await?;
-
-    // tracing::info!(
-    //     "Successfully started the internal RPC server: {}",
-    //     internal_rpc_url
-    // );
-
-    // tokio::spawn(async move {
-    //     internal_rpc_server.stopped().await;
-    // });
+    tokio::spawn(async move {
+        internal_rpc_server.stopped().await;
+    });
 
     Ok(())
 }
 
-async fn initialize_cluster_rpc_server(app_state: &AppState) -> Result<(), Error> {
-    // let cluster_rpc_url = app_state.config().cluster_rpc_url().to_string();
+async fn initialize_cluster_rpc_server(context: &AppState) -> Result<(), Error> {
+    let cluster_rpc_url = context.config().cluster_rpc_url().to_string();
 
-    // let sequencer_rpc_server = RpcServer::new(app_state)
-    //     .init(cluster_rpc_url.clone())
-    //     .await?;
+    let sequencer_rpc_server = RpcServer::new(context.clone())
+        .init(cluster_rpc_url.clone())
+        .await?;
 
-    // tracing::info!(
-    //     "Successfully started the cluster RPC server: {}",
-    //     cluster_rpc_url
-    // );
+    tracing::info!(
+        "Successfully started the cluster RPC server: {}",
+        cluster_rpc_url
+    );
 
-    // tokio::spawn(async move {
-    //     sequencer_rpc_server.stopped().await;
-    // });
+    tokio::spawn(async move {
+        sequencer_rpc_server.stopped().await;
+    });
 
     Ok(())
 }
 
-async fn initialize_external_rpc_server(app_state: &AppState) -> Result<JoinHandle<()>, Error> {
-    let sequencer_rpc_url = app_state.config().sequencer_rpc_url().to_string();
+async fn initialize_external_rpc_server(context: &AppState) -> Result<JoinHandle<()>, Error> {
+    let sequencer_rpc_url = context.config().sequencer_rpc_url().to_string();
 
     // Initialize the external RPC server.
-    // let external_rpc_server = RpcServer::new(app_state.clone())
-    //     .init(sequencer_rpc_url.clone())
-    //     .await?;
+    let external_rpc_server = RpcServer::new(context.clone())
+        .init(sequencer_rpc_url.clone())
+        .await?;
 
-    // tracing::info!(
-    //     "Successfully started the sequencer RPC server: {}",
-    //     sequencer_rpc_url
-    // );
+    tracing::info!(
+        "Successfully started the sequencer RPC server: {}",
+        sequencer_rpc_url
+    );
 
     let server_handle = tokio::spawn(async move {
-        // external_rpc_server.stopped().await;
+        external_rpc_server.stopped().await;
     });
 
     Ok(server_handle)
