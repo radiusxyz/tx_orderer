49c5cd224e0328ed2c5918dd3b79bb13d1671a31
stompesi <stompesi@gmail.com>
2024-09-11 15:10:51 +0900

    move debug module to internal module

--- src/rpc/internal/mod.rs
+++ src/rpc/internal/mod.rs
@@ -1,7 +1,9 @@
 mod add_sequencing_info;
+// mod debug;
 mod deregister;
 mod join_cluster;
 
 pub use add_sequencing_info::AddSequencingInfo;
+// pub use debug::*;
 pub use deregister::Deregister;
 pub use join_cluster::JoinCluster;
