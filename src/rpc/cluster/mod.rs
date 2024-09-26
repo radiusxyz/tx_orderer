f9cc763c62c0a337f0b0d14a9301181e55b0b868
Kanet <zombiedelah@gmail.com>
2024-09-26 15:30:07 +0900

    Remove 'SyncBlockMessage'

--- src/rpc/cluster/mod.rs
+++ src/rpc/cluster/mod.rs
@@ -2,6 +2,6 @@ mod sync_block;
 mod sync_encrypted_transaction;
 mod sync_raw_transaction;
 
-pub use sync_block::{SyncBlock, SyncBlockMessage};
+pub use sync_block::SyncBlock;
 pub use sync_encrypted_transaction::SyncEncryptedTransaction;
 pub use sync_raw_transaction::SyncRawTransaction;
