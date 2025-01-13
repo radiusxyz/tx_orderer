d92719ef69d32e15a4b4f2d79dc1bdfc7de01463
stompesi <stompesi@gmail.com>
2025-01-13 01:52:31 +0000

    Add default

--- src/types/order_commitment/order_commitment_type/sign_order_commitment.rs
+++ src/types/order_commitment/order_commitment_type/sign_order_commitment.rs
@@ -8,14 +8,26 @@ pub struct SignOrderCommitment {
     pub signature: String,
 }
 
-#[derive(Clone, Default, Debug, Deserialize, Serialize)]
-
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct OrderCommitmentData {
     pub rollup_id: String,
     pub block_height: u64,
     pub transaction_order: u64,
 
-    #[serde(serialize_with = "serialize_merkle_path")]
-    #[serde(deserialize_with = "deserialize_merkle_path")]
+    #[serde(
+        serialize_with = "serialize_merkle_path",
+        deserialize_with = "deserialize_merkle_path"
+    )]
     pub pre_merkle_path: Vec<[u8; 32]>,
+}
+
+impl Default for OrderCommitmentData {
+    fn default() -> Self {
+        Self {
+            rollup_id: String::new(),
+            block_height: 0,
+            transaction_order: 0,
+            pre_merkle_path: Vec::new(),
+        }
+    }
 }
