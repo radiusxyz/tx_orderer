59018ecdf06fe783e6a615fbd42da4b821e68adf
kanet1105 <zombiedelah@gmail.com>
2025-02-04 16:18:35 +0900

    Change index iteration

--- src/merkle_tree_manager.rs
+++ src/merkle_tree_manager.rs
@@ -33,7 +33,6 @@ impl MerkleTreeManager {
             let merkle_tree = MerkleTree::new();
 
             if let Some(rollup_metadata) = RollupMetadata::get(rollup_id).ok() {
-                let transaction_order = rollup_metadata.transaction_order;
                 for index in 0..rollup_metadata.transaction_order {
                     let (raw_transaction, _) = RawTransactionModel::get(
                         rollup_id,
