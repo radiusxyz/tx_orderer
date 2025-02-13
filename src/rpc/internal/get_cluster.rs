5f99bbb09681b7032aedab21b35e1e46e695dbfc
stompesi <stompesi@gmail.com>
2025-02-13 05:23:48 +0000

    Change return value in get_cluster function

--- src/rpc/internal/get_cluster.rs
+++ src/rpc/internal/get_cluster.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use crate::rpc::prelude::*;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -5,13 +7,11 @@ pub struct GetCluster {
     pub platform: Platform,
     pub service_provider: ServiceProvider,
     pub cluster_id: String,
-    pub platform_block_height: Option<u64>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct GetClusterResponse {
-    pub cluster: Cluster,
-    pub latest_cluster_block_height: u64,
+    pub cluster_info: BTreeMap<u64, Cluster>,
 }
 
 impl RpcParameter<AppState> for GetCluster {
@@ -21,29 +21,43 @@ impl RpcParameter<AppState> for GetCluster {
         "get_cluster"
     }
 
-    async fn handler(self, _context: AppState) -> Result<Self::Response, RpcError> {
-        let platform_block_height = if self.platform_block_height.is_none() {
-            LatestClusterBlockHeight::get_or(
-                self.platform,
-                self.service_provider,
-                &self.cluster_id,
-                LatestClusterBlockHeight::default,
-            )?
-            .get_block_height()
-        } else {
-            self.platform_block_height.unwrap()
-        };
-
-        let cluster = Cluster::get(
+    async fn handler(self, context: AppState) -> Result<Self::Response, RpcError> {
+        let latest_cluster_block_height = LatestClusterBlockHeight::get_or(
             self.platform,
             self.service_provider,
             &self.cluster_id,
-            platform_block_height,
-        )?;
+            LatestClusterBlockHeight::default,
+        )?
+        .get_block_height();
 
-        Ok(GetClusterResponse {
-            cluster,
-            latest_cluster_block_height: platform_block_height,
-        })
+        let liveness_service_manager_client = context
+            .get_liveness_service_manager_client::<
+                liveness_service_manager::radius::LivenessServiceManagerClient,
+            >(self.platform, self.service_provider)
+            .await?;
+
+        let mut block_margin: u64 = liveness_service_manager_client
+            .publisher()
+            .get_block_margin()
+            .await
+            .expect("Failed to get block margin")
+            .try_into()
+            .expect("Failed to convert block margin");
+
+        let mut cluster_info = BTreeMap::new();
+        while block_margin > 0 {
+            let platform_block_height = latest_cluster_block_height - block_margin;
+            let cluster = Cluster::get(
+                self.platform,
+                self.service_provider,
+                &self.cluster_id,
+                platform_block_height,
+            )?;
+
+            cluster_info.insert(platform_block_height, cluster);
+            block_margin -= 1;
+        }
+
+        Ok(GetClusterResponse { cluster_info })
     }
 }
