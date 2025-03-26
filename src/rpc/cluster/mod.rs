165464cbc27c0e2a3bcad0b25232a655c4a127e3
stompesi <stompesi@gmail.com>
2025-03-26 00:05:55 +0000

    Add sync leader

--- src/rpc/cluster/mod.rs
+++ src/rpc/cluster/mod.rs
@@ -1,9 +1,13 @@
 mod get_raw_transaction_list;
+mod set_max_gas_limit;
 mod sync_encrypted_transaction;
+mod sync_leader;
 mod sync_max_gas_limit;
 mod sync_raw_transaction;
 
 pub use get_raw_transaction_list::*;
+pub use set_max_gas_limit::*;
 pub use sync_encrypted_transaction::*;
+pub use sync_leader::*;
 pub use sync_max_gas_limit::*;
 pub use sync_raw_transaction::*;
