1353a0df0a8a402af50d3fa0107509ced113597f
stompesi <stompesi@gmail.com>
2025-04-14 06:23:32 +0000

    Add get_order_commitment_info api

---
+++ src/rpc/cluster/get_order_commitment_info.rs
@@ -0,0 +1,42 @@
+use radius_sdk::json_rpc::server::ProcessPriority;
+
+use crate::rpc::prelude::*;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetOrderCommitmentInfo {
+    pub rollup_id: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetOrderCommitmentInfoResponse {
+    pub batch_number: u64,
+    pub transaction_order: u64,
+}
+
+impl RpcParameter<AppState> for GetOrderCommitmentInfo {
+    type Response = GetOrderCommitmentInfoResponse;
+
+    fn method() -> &'static str {
+        "get_order_commitment_info"
+    }
+
+    fn priority(&self) -> ProcessPriority {
+        ProcessPriority::High
+    }
+
+    async fn handler(self, _context: AppState) -> Result<Self::Response, RpcError> {
+        let rollup_metadata = RollupMetadata::get(&self.rollup_id)?;
+
+        tracing::info!(
+            "Get order commitment info: rollup_id = {}, batch_number = {}, transaction_order = {}",
+            self.rollup_id,
+            rollup_metadata.batch_number,
+            rollup_metadata.transaction_order
+        );
+
+        Ok(GetOrderCommitmentInfoResponse {
+            batch_number: rollup_metadata.batch_number,
+            transaction_order: rollup_metadata.transaction_order,
+        })
+    }
+}
--- src/rpc/cluster/mod.rs
+++ src/rpc/cluster/mod.rs
@@ -1,4 +1,5 @@
 mod create_batch;
+mod get_order_commitment_info;
 mod get_raw_transaction_list;
 mod set_max_gas_limit;
 mod sync_encrypted_transaction;
@@ -8,6 +9,7 @@ mod sync_max_gas_limit;
 mod sync_raw_transaction;
 
 pub use create_batch::*;
+pub use get_order_commitment_info::*;
 pub use get_raw_transaction_list::*;
 pub use set_max_gas_limit::*;
 pub use sync_encrypted_transaction::*;
